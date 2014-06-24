e209c7c77e20b5f10d3a0f35a9932fa3ff7618b0
Kelly Xu <kellyx97@gmail.com>
2014-06-24 13:51:39 -0700

    Automatically Committed for MakeGamesWithUs

--- GameOfLife.spritebuilder/Grid.m
+++ GameOfLife.spritebuilder/Grid.m
@@ -163,41 +163,18 @@ static const int GRID_COLUMNS = 10;
             // access the creature in the cell that corresponds to the current row/column
             Creature *currentCreature = _gridArray[i][j];
             
-            // remember that every creature has a 'livingNeighbors' property that we created earlier
-            //currentCreature.livingNeighbors = 0;
-            
-            // now examine every cell around the current one
-            
-            // go through the row on top of the current cell, the row the cell is in, and the row past the current cell
-            //for (int x = (i-1); x <= (i+1); x++)
-            //{
-                // go through the column to the left of the current cell, the column the cell is in, and the column to the right of the current cell
-                //for (int y = (j-1); y <= (j+1); y++)
-                //{
-                    // check that the cell we're checking isn't off the screen
-                    //BOOL isIndexValid;
-                    //isIndexValid = [self isIndexValidForX:x andY:y];
-                    
-                    // skip over all cells that are off screen AND the cell that contains the creature we are currently updating
-                    //if (!((x == i) && (y == j)) && isIndexValid)
-                    //{
-                        //Creature *neighbor = _gridArray[x][y];
-                        if (currentCreature.livingNeighbors == 3)
-                        {
-                            currentCreature.isAlive = YES;
-                            numAlive += 1;
+                if (currentCreature.livingNeighbors == 3)
+                    {
+                        currentCreature.isAlive = YES;
+                        numAlive += 1;
                             //currentCreature.livingNeighbors += 1;
-                        }
-                        else if (currentCreature.livingNeighbors <= 1 ||currentCreature.livingNeighbors >= 4)
-                        {
-                            currentCreature.isAlive = NO;
-                        }
-                    //}
+                    }
+                else if (currentCreature.livingNeighbors <= 1 ||currentCreature.livingNeighbors >= 4)
+                    {
+                        currentCreature.isAlive = NO;
+                    }
                 }
             }
-        //}
-    //}
     _totalAlive = numAlive;
-
 }
 @end
