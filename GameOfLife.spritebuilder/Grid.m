84cf9d2c76a944e5c61482b1b9f1fd205ecf9dfc
Kelly Xu <kellyx97@gmail.com>
2014-06-24 14:02:37 -0700

    Automatically Committed for MakeGamesWithUs

--- GameOfLife.spritebuilder/Grid.m
+++ GameOfLife.spritebuilder/Grid.m
@@ -161,7 +161,7 @@ static const int GRID_COLUMNS = 10;
         {
             // access the creature in the cell that corresponds to the current row/column
             Creature *currentCreature = _gridArray[i][j];
-            
+            numAlive++;
                 if (currentCreature.livingNeighbors == 3)
                     {
                         currentCreature.isAlive = YES;
